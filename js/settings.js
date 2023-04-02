1a20bb77dc3dc95eb34dcebc2e46bd75bc3bbb6c
Dmitriy <87428235+DmitriySido@users.noreply.github.com>
2023-04-02 18:39:40 +0300

    Add files via upload

--- js/settings.js
+++ js/settings.js
@@ -133,3 +133,21 @@ let enableScroll = function () {
 	settingsPopup.removeAttribute('data-position');
 }
 
+/*Video*/
+
+let video = document.querySelector('.video');
+const circles = document.querySelector('.video-circle');
+const triangle = document.querySelector('.video-triangle');
+
+video.addEventListener('click', function(){
+	video.insertAdjacentHTML('afterbegin', '<iframe src="https://www.youtube.com/embed/YVgfHZMFFFQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
+
+	if(video.classList.contains('ready')){
+		return;
+	}
+
+	video.classList.add('ready');
+	circles.style.display = "none";
+	triangle.style.display = "none";
+
+})
